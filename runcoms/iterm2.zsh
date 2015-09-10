325ece5499408c7451989facb446d3bf2c9603ab
Dave Stibrany <daves@hubdoc.com>
2015-09-10 11:06:52 -0400

    ssh tab colors for prod servers

--- runcoms/iterm2.zsh
+++ runcoms/iterm2.zsh
@@ -17,8 +17,9 @@ tab-reset() {
 # reset the color after the connection closes
 color-ssh() {
     if [[ -n "$ITERM_SESSION_ID" ]]; then
+
         trap "tab-reset" INT EXIT
-        if [[ "$*" =~ "production|ec2-.*compute-1" ]]; then
+        if [[ "$*" =~ "app1|db1" ]]; then
             tab-color 255 0 0
         else
             tab-color 0 255 0
